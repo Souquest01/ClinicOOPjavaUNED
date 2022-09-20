2568841ad82e2b93d00b3c5ff2add96f7be3d5f8
Souquest01 <100442802+Souquest01@users.noreply.github.com>
2022-09-20 23:16:40 +0100

    anhadida clase main

---
+++ clinica-copy/Main.java
@@ -0,0 +1,13 @@
+ 
+/**
+ * Write a description of class Main here.
+ *
+ * @author (Francisco Sanchez)
+ * @version (a version number or a date)
+ */
+public class Main {
+    public static void main(String[] args) {
+        Clinica x = new Clinica();
+        x.iniciar();
+    }
+}
